59879195c40e738ade54bb3aaf43a25cc65c4512
Andrii-Detix <aivancisin6@gmail.com>
2025-09-18 11:30:23 +0300

    Add lab-1

---
+++ lab-1/masm32.asm
@@ -0,0 +1,25 @@
+﻿.486
+.model flat, stdcall
+option casemap: none
+;========================
+include \masm32\include\user32.inc
+include \masm32\include\windows.inc
+include \masm32\include\kernel32.inc
+
+includelib \masm32\lib\user32.lib
+includelib \masm32\lib\kernel32.lib
+;========================
+.data
+	MessageBoxTitle db			"Lab 1 - masm32", NULL
+	MessageBoxInformation db 	"ПІБ: Іванчишин Андрій Олександрович", 13, 10,
+								"Заліковка: відсутня", 13, 10,
+								"Номер в списку групи: 8", NULL
+
+.code
+main:
+
+	invoke MessageBox, NULL, offset MessageBoxInformation, offset MessageBoxTitle, MB_OK
+	;--------------------
+	invoke ExitProcess, NULL
+
+end main
---
+++ lab-1/masm64.asm
@@ -0,0 +1,33 @@
+﻿option casemap: none
+;===================
+include \masm64\include\win64.inc
+include \masm64\include\kernel32.inc
+include \masm64\include\user32.inc
+
+includelib \masm64\lib\kernel32.lib
+includelib \masm64\lib\user32.lib
+;===================
+.data
+	MessageBoxTitle db			"Lab 1 - masm64", NULL
+	MessageBoxInformation db 	"ПІБ: Іванчишин Андрій Олександрович", 13, 10,
+								"Заліковка: відсутня", 13, 10,
+								"Номер в списку групи: 8", NULL
+
+.code
+Main proc
+
+    sub rsp, 8h
+    ;---------------
+    mov rcx, NULL                    
+    lea rdx, MessageBoxInformation            
+    lea r8,  MessageBoxTitle        
+    mov r9d, MB_OK                    
+    ;---------------
+    call     MessageBox                
+    ;---------------
+    mov rcx, NULL    
+    ;---------------
+    call     ExitProcess     
+    
+Main endp
+end
